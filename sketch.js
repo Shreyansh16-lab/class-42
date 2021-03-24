21cc05795c1e49e2d74313de80a5162bb017c8e6
Shreyansh16-lab <shreyanshgoswami90@gmail.com>
2021-03-24 16:45:26 +0530

    modified

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 var canvas, backgroundImage;
 
-var gameState = 0,finishedPlayers;
+var gameState = 0,
+    finishedPlayers;
 var playerCount;
 var allPlayers;
 var distance = 0;
@@ -10,47 +11,61 @@ var form, player, game;
 
 var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
+var f1, f1Img;
+var w, h;
+var obstacles; //there is a problem in game . js 
+var sliding;
 
-function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
+function preload() {
+    track = loadImage("images/track.jpg");
+    car1_img = loadImage("images/car1.png");
+    car2_img = loadImage("images/car2.png");
+    car3_img = loadImage("images/car3.png");
+    car4_img = loadImage("images/car4.png");
+    ground = loadImage("images/ground.png");
+    f1Img = loadImage("images/f1.png");
+    sliding = loadSound("sound/sliding.mp3");
 }
 
-function setup(){
-  canvas = createCanvas(displayWidth , displayHeight);
-  database = firebase.database();
-  gameState = 0;
-  distance = 0;
- // finishedPlayers = 0;
-  yVel = 0;
-  xVel = 0;
+function setup() {
+    canvas = createCanvas(displayWidth, displayHeight);
+    database = firebase.database();
+    gameState = 0;
+    distance = 0;
+    // finishedPlayers = 0;
+    yVel = 0;
+    xVel = 0;
+    obstacles = createGroup();
 
-  xSet = false;
-  game = new Game();
-  game.getState();
-  game.start();
+    for (var i = 0; i < 5; i++) {
+        w = random(200, 900);
+        h = random(-height * 4, height - 300);
+        f1 = createSprite(w, h);
+        f1.addImage("f1", f1Img);
+        obstacles.add(f1);
+    }
+
+    xSet = false;
+    game = new Game();
+    game.getState();
+    game.start();
 }
 
 
-function draw(){
-   //start the game
-   background(200, 200, 255);
+function draw() {
+    //start the game
+    background(200, 200, 255);
 
-   //start the game
-   if (playerCount === 4 ) {
-     game.update(1);
-   }
- 
-   //start the game for real
-   if (gameState === 1) {
-     game.play();
-   }
-   if (gameState === 2) {
-     console.log("End");
-   }}
- 
-  
+    //start the game
+    if (playerCount === 4) {
+        game.update(1);
+    }
+
+    //start the game for real
+    if (gameState === 1) {
+        game.play();
+    }
+    if (gameState === 2) {
+        console.log("End");
+    }
+}
